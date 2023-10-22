538cf79d435db7b59d86540ee4b3813519785d6f
moooove08 <tj.vlad@gmail.com>
2023-10-22 18:28:21 +0300

    Initial Commit

---
+++ WhichYouAnimal/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhichYouAnimal
+//
+//  Created by Vlad Kuzmenko on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
